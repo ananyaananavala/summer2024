6e167e65a35be0f8eb9bcf3aa5866b6eab9be04c
ananyaananavala <165343856+ananyaananavala@users.noreply.github.com>
2024-07-15 11:18:17 -0500

    Update lowest-highest.py
    added two comments

--- lowest-highest.py
+++ lowest-highest.py
@@ -1,9 +1,10 @@
 # print the smalles and highest value in array
 mylist = [1,6,8,4,0]
-mylist.sort()
-print(mylist)
+mylist.sort()         # sorting
+print(mylist)         
 
 print("the lowest number is: ", mylist[0])
 print("the highest number is: ", mylist[-1])
 
 #edit edit
+#hello world
