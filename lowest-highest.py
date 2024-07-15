38f36545546c183decb34073bdff52726d13d8b0
ananyaananavala <165343856+ananyaananavala@users.noreply.github.com>
2024-07-15 11:15:13 -0500

    Create lowest-highest.py
    added a file for printing lowest and highest number

---
+++ lowest-highest.py
@@ -0,0 +1,7 @@
+# print the smalles and highest value in array
+mylist = [1,6,8,4,0]
+mylist.sort()
+print(mylist)
+
+print("the lowest number is: ", mylist[0])
+print("the highest number is: ", mylist[-1])
